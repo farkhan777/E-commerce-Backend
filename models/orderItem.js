e44b85c8c9b8efeb75f98e059d5e12d2fbc2c642
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-09-01 13:37:51 +0700

    create order and order item

---
+++ models/orderItem.js
@@ -0,0 +1,22 @@
+const { default: mongoose } = require('mongoose')
+
+const orderItemSchema = mongoose.Schema({
+    quantity: {
+        type: Number,
+        require: true
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }
+})
+
+orderItemSchema.virtual('id').get(function () {
+    return this._id.toHexString()
+})
+
+orderItemSchema.set('toJSON', {
+    virtuals: true
+})
+
+module.exports = mongoose.model('OrderItem', orderItemSchema)
--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const order = require('../models/order')
 const router = express.Router()
 const Order = require('../models/order')
+const OrderItem = require('../models/orderItem')
 
 router.get(`/`, async (req, res) => {
-    const getAllItems = await Order.find()
+    const getAllItems = await Order.find().sort({dateOrdered: -1}).populate('user', "name")
 
     if (!getAllItems) {
         res.status(500).json({
@@ -12,6 +14,67 @@ router.get(`/`, async (req, res) => {
     }
 
     res.send(getAllItems)
+})
+
+router.get(`/:id`, async (req, res) => {
+    const getAllItems = await Order.findById(req.params.id)
+    .populate('user', "name")
+    .populate({
+        path: 'orderItems', populate: {
+            path: 'product', populate: 'category'
+        }
+    })
+
+    if (!getAllItems) {
+        res.status(500).json({
+            success: false
+        })
+    }
+
+    res.send(getAllItems)
+})
+
+router.post(`/`, async (req, res) => {
+
+    const orderItemsIds = Promise.all(req.body.orderItems.map( async (orderitem) => {
+        const newOrderItem = new OrderItem({
+            quantity: orderitem.quantity,
+            product: orderitem.product
+        })
+
+        await newOrderItem.save()
+
+        return newOrderItem.id
+    }))
+
+    const orderItemsIdsResolved = await orderItemsIds
+
+    console.log(orderItemsIdsResolved)
+
+    const newOrder = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user,
+    })
+
+    try {    
+        await newOrder.save()
+    
+        if (!newOrder) {
+            return res.status(500).json({message: 'The order can not be saved'})
+        }
+    } catch (err) {
+        console.log(err)
+    }
+    
+    res.send(newOrder)
 })
 
 module.exports = router
